f53b193e30c306333c1c1e2bd4275fb4d5115661
RobertaMatera <materaroberta11@gmail.com>
2022-02-28 19:16:33 +0100

    add edit/delete column + style -> table

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -16,6 +16,7 @@ export class TableComponent implements OnInit {
     'productFreshness',
     'productPrice',
     'productComment',
+    'editDelete'
   ];
   dataSource!: MatTableDataSource<any>;
 
